6608ad34b7b6a2830d2a24008cc6ad301eacc7f1
Aleksandr Charusgin <charushinas@gmail.com>
2026-06-24 20:07:46 +0300

    refactor (tests): extract LoginPage and ProductsPage, update BaseTest and LoginTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,23 +1,38 @@
+package tests;
+
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+import java.time.Duration;
 
 public class BaseTest {
     WebDriver driver;
+    LoginPage loginPage;
+    ProductsPage productsPage;
 
     @BeforeMethod
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
+        options.addArguments("--guest");
         options.addArguments("headless");
         driver = new ChromeDriver(options);
-        driver.get("https://www.saucedemo.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+        driver.manage().window().setSize(new Dimension(800, 600));
+
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
     }
 
     @AfterMethod
     public void close() {
+        driver.manage().deleteAllCookies();
         driver.quit();
     }
 }
